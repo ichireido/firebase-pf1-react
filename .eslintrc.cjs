ef9cddaea27c1dcc80669927a8d33fe58af7be27
ichireido <ichireido@gmail.com>
2024-05-02 15:14:44 +0900

    微修正

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,7 +15,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
   },
 };

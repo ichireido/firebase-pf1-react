f6c0ded23b48a689adb0185e002a4dbee7a598b3
ichireido <ichireido@gmail.com>
2024-05-04 10:59:13 +0900

    ビルドエラー対応

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const App: React.FunctionComponent = () => {
+// const App: React.FunctionComponent = () => {
+const App = () => {
   const [count, setCount] = useState(0)
 
   return (
--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -27,6 +27,7 @@ interface ButtonProps {
 /**
  * Primary UI component for user interaction
  */
+// export const Button: React.FC<ButtonProps> = ({
 export const Button = ({
   primary = false,
   size = 'medium',

a2cda7b2f2011482b939fed72facb63255fbb05b
ichireido <ichireido@gmail.com>
2024-05-23 14:44:25 +0900

    スクロール不具合対応（position:stickyを一旦やめる）

--- src/pf1App.tsx
+++ src/pf1App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 // import React from 'react';
 import './pf1App.css';
 // import { Link as LinkScroll } from 'react-scroll';
@@ -46,6 +46,13 @@ import portfolios from '../portfolios.json';
 // console.log('storageRef', storageRef);
 
 const pf1App: React.FunctionComponent = () => {
+  const linktRef = portfolios.map((val) => {
+    return {
+      ...val,
+      ref: useRef<HTMLDivElement | null>(null),
+    };
+  });
+
   const works = workExperience.map((val) => {
     const datailList = val.detailList.map((val, index) => {
       return <li key={index}>{val.detail}</li>;
@@ -62,14 +69,7 @@ const pf1App: React.FunctionComponent = () => {
     );
   });
 
-  const listRef = portfolios.map((val) => {
-    return {
-      ...val,
-      ref: useRef<HTMLDivElement>(null),
-    };
-  });
-
-  const portfolio = listRef.map((val, index) => {
+  const portfolio = linktRef.map((val, index) => {
     // const idRef = useRef<HTMLDivElement>(null);
     // if (idRef !== null || idRef !== undefined) {
     //   console.log('idRef-', val, ':', idRef.current?.scrollIntoView());
@@ -79,76 +79,91 @@ const pf1App: React.FunctionComponent = () => {
       return <p key={index}>{val.detail}</p>;
     });
 
-    return (
-      <div
-        key={val.id}
-        id={val.id}
-        ref={val.ref}
-        className={`mainblock sticky performance${index + 1}`}
-      >
-        {/* <div className="navenpty">
-                </div> */}
-        <div className={`mainblocksection bg-${val.bg}`}>
-          <section className={`tilesection appear-totop text-${val.txt}`}>
-            <a className="imglink" href={val.href} target="_new">
-              <img className={`site-img performance${index + 1}`} />
-            </a>
-            <h3 className="sakuhinh3">
-              <a href={val.href} target="_new">
-                {val.title}
-              </a>
-            </h3>
-            {datailList}
+    if (val.id === 'top') {
+      return (
+        <div
+          id={val.id}
+          key={val.id}
+          className="mainblock prologuetop"
+          ref={val.ref}
+        >
+          {/* <div className="navenpty">
+              </div> */}
+          <section className="mainblocksection text-white">
+            <h1>PORTFOLIO</h1>
           </section>
         </div>
-      </div>
-    );
-  });
-
-  const portfolioLink = [
-    {
-      id: 'top',
-      bg: '',
-      txt: '',
-      href: '',
-      imgSrc: '',
-      title: '',
-      detailList: [{ detail: '' }],
-    },
-    {
-      id: 'bio',
-      bg: '',
-      txt: '',
-      href: '',
-      imgSrc: '',
-      title: '',
-      detailList: [{ detail: '' }],
-    },
-  ];
-  portfolioLink.push(...portfolios);
-
-  const linktRef = portfolioLink.map((val) => {
-    return {
-      ...val,
-      ref: useRef<HTMLDivElement | null>(null),
-    };
+      );
+    } else if (val.id === 'bio') {
+      return [
+        <>
+          <div
+            id={val.id}
+            key={val.id}
+            className="mainblock prologuetop"
+            ref={val.ref}
+          >
+            {/* <div className="navenpty">
+                </div> */}
+            <div className="mainblocksection bg-black">
+              <section className="biosection">{works}</section>
+            </div>
+          </div>
+          <div id="heading1" className="mainblock sticky heading">
+            {/* <div className="navenpty">
+                </div> */}
+            <section className="mainblocksection">
+              <h2>
+                デザイン実績、作品紹介 <br />
+                (リンク先に制作物あり)
+              </h2>
+            </section>
+          </div>
+        </>,
+      ];
+    } else {
+      return (
+        <div
+          id={val.id}
+          key={val.id}
+          className={`mainblock sticky performance${index - 1}`}
+          ref={val.ref}
+        >
+          {/* <div className="navenpty">
+                  </div> */}
+          <div className={`mainblocksection bg-${val.bg}`}>
+            <section className={`tilesection appear-totop text-${val.txt}`}>
+              <a className="imglink" href={val.href} target="_new">
+                <img className={`site-img performance${index - 1}`} />
+              </a>
+              <h3 className="sakuhinh3">
+                <a href={val.href} target="_new">
+                  {val.title}
+                </a>
+              </h3>
+              {datailList}
+            </section>
+          </div>
+        </div>
+      );
+    }
   });
 
   console.log('linktRef', linktRef);
 
-  const handleClick = (ref) => {
-    console.log('linktRef-click', linktRef);
+  const handleClick = (ref: React.MutableRefObject<HTMLDivElement | null>) => {
+    console.log('ref-click', ref, linktRef);
     // useEffect(() => {
     //   console.log(ref.current);
     // });
-    if (!ref.current) return;
-    ref.current.scrollIntoView({ behavior: 'smooth' });
-    console.log('linktRef-click-after', linktRef);
+
+    if (ref.current) {
+      ref.current.scrollIntoView({ behavior: 'smooth' });
+    } else return;
+    console.log('ref-click-after', ref, linktRef);
   };
 
   const navList = linktRef.map((val) => {
-    // const idRef = useRef<HTMLDivElement>(null);
-
     return (
       <>
         {/* <LinkScroll
@@ -158,14 +173,17 @@ const pf1App: React.FunctionComponent = () => {
           duration={850}
         > */}
         {/* <a key={val.id} className="nav-link" href={`#${val.id}`}> */}
-        <p
+        <section
           key={val.id}
           className="nav-link"
-          onClick={() => handleClick(val.ref)}
+          onClick={() => {
+            console.log('clicked!');
+            handleClick(val.ref);
+          }}
         >
           {val.id}
-          {/* </a> */}
-        </p>
+        </section>
+        {/* </a> */}
         {/* </LinkScroll> */}
       </>
     );
@@ -182,54 +200,7 @@ const pf1App: React.FunctionComponent = () => {
     <div id="wrapper">
       <article id="main">
         <div className="nav">
-          <nav className="nav-inner">
-            {/* <LinkScroll
-              to="top"
-              className="nav-link"
-              smooth={true}
-              duration={850}
-            > */}
-            {/* <a id="nav1" className="nav-link" href="#top" key="top">
-              top
-            </a> */}
-            {/* </LinkScroll> */}
-            {/* <LinkScroll
-              to="bio"
-              className="nav-link"
-              smooth={true}
-              duration={850}
-            > */}
-            {/* <a id="nav1_5" className="nav-link" href="#bio" key="bio">
-              bio
-            </a> */}
-            {/* </LinkScroll> */}
-            {navList}
-          </nav>
-        </div>
-
-        <div id="top" className="mainblock prologuetop">
-          {/* <div className="navenpty">
-                </div> */}
-          <section className="mainblocksection text-white">
-            <h1>PORTFOLIO</h1>
-          </section>
-        </div>
-        <div id="bio" className="mainblock prologuetop">
-          {/* <div className="navenpty">
-                </div> */}
-          <div className="mainblocksection bg-black">
-            <section className="biosection">{works}</section>
-          </div>
-        </div>
-        <div id="heading1" className="mainblock sticky heading">
-          {/* <div className="navenpty">
-                </div> */}
-          <section className="mainblocksection">
-            <h2>
-              デザイン実績、作品紹介 <br />
-              (リンク先に制作物あり)
-            </h2>
-          </section>
+          <nav className="nav-inner">{navList}</nav>
         </div>
         {portfolio}
         <div id="footer" className="mainblock sticky footer">
